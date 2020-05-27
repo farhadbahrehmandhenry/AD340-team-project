ace1ec75e3c493647b1378966ce4ab854033be59
theKunte <jennykunte90@gmail.com>
2020-05-26 18:49:58 -0700

    changed design for create_account_activity layout

--- app/src/main/java/com/example/ad340_team_project/CreateAccountActivity.java
+++ app/src/main/java/com/example/ad340_team_project/CreateAccountActivity.java
@@ -1,6 +1,9 @@
 package com.example.ad340_team_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAccountActivity extends AppCompatActivity {
@@ -10,4 +13,9 @@ public class CreateAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account_activity);
     }
+
+//    public void createAccount(View view) {
+//        Intent intent = new Intent(this, MainActivity.class);
+//        finish();
+//    }
 }
