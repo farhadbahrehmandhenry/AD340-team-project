ca61c192c41388691f7ac858b3416300c7d92d2f
theKunte <jennykunte90@gmail.com>
2020-06-06 23:03:54 -0700

    changed fragment_profile placeholder and added ProfileFragmentPageAdapter

--- app/src/main/java/com/example/ad340_team_project/ProfileFragment.java
+++ app/src/main/java/com/example/ad340_team_project/ProfileFragment.java
@@ -13,5 +13,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
             return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
         }
 }
