18a6613a2d41ad1a273488259ad60fad89dbc764
Jak Ashuraliev <ashuraliev@gmail.com>
2020-06-15 16:57:08 -0700

    clean up code, strings, deleted unused imports and files

--- app/src/main/java/com/example/ad340_team_project/HostActivity.java
+++ app/src/main/java/com/example/ad340_team_project/HostActivity.java
@@ -2,10 +2,7 @@ package com.example.ad340_team_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
--- app/src/main/java/com/example/ad340_team_project/ProfileActivity.java
+++ app/src/main/java/com/example/ad340_team_project/ProfileActivity.java
@@ -2,7 +2,6 @@ package com.example.ad340_team_project;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
